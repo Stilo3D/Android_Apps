3b1e528ffa5d509b0459246f2175e1eee60abcae
speeddrift <speeddriftt@gmail.com>
2022-10-16 11:45:56 +0200

    Second app - Quiz with rotation and additional options

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,13 +1,8 @@
 
 @
 app/build.gradle,f\4\f4a01d6a4fcb971362ec00a83903fd3902f52164
-:
-
-.gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
 ?
 settings.gradle,0\5\05efc8b1657769a27696d478ded1e95f38737233
->
-app/.gitignore,5\1\51e1c5d383dfaa35e0e7e5873a0a99355a86880f
 A
 gradle.properties,2\a\2afbb999f001938c88fa43fc2ef52abf0f8213e4
 <
@@ -24,3 +19,7 @@ z
 Japp/src/androidTest/java/com/example/firstapp/ExampleInstrumentedTest.java,1\c\1c83f12466097ddeee5bb4e4bc9adf1026d9726f
 k
 ;app/src/test/java/com/example/firstapp/ExampleUnitTest.java,9\e\9eeae27b4d63705c9a2280af5404f27eb8e2257f
+d
+4app/src/main/java/com/example/firstapp/Question.java,d\4\d446db537a08fac19703eeaad7e21ab246053ad9
+@
+app/TXT file.txt,8\b\8bcf005c13f0b0646060b4dfa72506399bf923df
--- app/src/main/java/com/example/firstapp/MainActivity.java
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.firstapp;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -13,29 +18,91 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView questionTextView;
     private int currentIndex = 0;
-
-
+    boolean answerWasShown;
+    private static final int REQUEST_CODE_PROMPT = 0;
+    private static final String KEY_CURRENT_INDEX = "currentIndex";
+    public static final String KEY_EXTRA_ANSWER = "com.example.firstapp.correctAnswer";
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("create", "onCreate executed");
+
         setContentView(R.layout.activity_main);
         Button trueButton;
         Button falseButton;
         Button nextButton;
+        Button showAnswerButton;
 
         trueButton = findViewById(R.id.true_button);
         falseButton = findViewById(R.id.false_button);
         nextButton = findViewById(R.id.next_button);
         questionTextView = findViewById(R.id.question_text_view);
+        showAnswerButton = findViewById(R.id.answer_button);
 
         trueButton.setOnClickListener(v -> checkAnswerCorrectness(true));
         falseButton.setOnClickListener(v -> checkAnswerCorrectness(false));
-        nextButton.setOnClickListener(v -> {currentIndex = (currentIndex + 1 ) % questions.length; setNextQuestion();});
+        nextButton.setOnClickListener(v -> {currentIndex = (currentIndex + 1 ) % questions.length; answerWasShown = false; setNextQuestion();});
+        
+        showAnswerButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, PromptActivity.class);
+            boolean correctAnswer = questions[currentIndex].isTrueAnswer();
+            intent.putExtra(KEY_EXTRA_ANSWER, correctAnswer);
+            startActivityForResult(intent, REQUEST_CODE_PROMPT);
+        });
 
+        if (savedInstanceState != null)
+            currentIndex = savedInstanceState.getInt(KEY_CURRENT_INDEX);
 
         setNextQuestion();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("start", "onStart executed");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("destroy", "onDestroy executed");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("stop", "onStop executed");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("pause", "onPause executed");
+    }
 
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("resume", "onResume executed");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.d("save", "Method SAVEINSTANCESTATE executed");
+        outState.putInt(KEY_CURRENT_INDEX, currentIndex);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode != RESULT_OK) {return;}
+        if (requestCode == REQUEST_CODE_PROMPT){
+            if (data == null) {return;}
+            answerWasShown = data.getBooleanExtra(PromptActivity.KEY_EXTRA_ANSWER_SHOWN, false);
+        }
     }
 
     private Question[] questions = new Question[] {
@@ -55,11 +122,15 @@ public class MainActivity extends AppCompatActivity {
     private void checkAnswerCorrectness(boolean userAnswer){
         boolean correctAnswer = questions[currentIndex].isTrueAnswer();
         int resultMessageId = 0;
-        if (userAnswer == correctAnswer) {
-            resultMessageId = R.string.correct_answer;
+        if (answerWasShown){
+            resultMessageId = R.string.answer_was_shown;
         }
-        else{
-            resultMessageId = R.string.incorrect_answer;
+        else {
+            if (userAnswer == correctAnswer) {
+                resultMessageId = R.string.correct_answer;
+            } else {
+                resultMessageId = R.string.incorrect_answer;
+            }
         }
         Toast.makeText(this, resultMessageId, Toast.LENGTH_SHORT).show();
     }
